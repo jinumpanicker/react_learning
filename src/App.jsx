26d58c2ca5db9c4ebbdce13d740a2c84cfc498ee
Jinu M Panicker <jinumpanicker@gmail.com>
2025-07-21 00:36:22 +0530

    Day 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
-function getTitle(greeting,title){
-    return greeting + " " + title;
-}
-
- 
+const list =[
+  {
+    title: 'Bhushita',
+    url:'bhushita.com',
+    father: 'Hari inamadugu'
+  },
+  {
+    title: 'Sai Ashrita',
+    url:'inamadugu.com',
+    father: 'Sri Hari Reddy Inamadugu'
+  },
+];
 
 function App() {
 
 
   return (
-    <div><h1>
-       {getTitle("Hi","Bhushita")} </h1>
+    <div><h1>My hacker Stories </h1>
 
       <label htmlFor="search"> Search: </label>
       <input id="search" type="text" / >
+      <hr/>
+      <ul>
+  {list.map(function(item, idx) {
+    return (
+      <li key={idx}>
+        {item.title} - {item.father}
+      </li>
+    );
+  })}
+</ul>
       </div>
   );
 }
